e204654ef8c8d4f11b944bcea5b2b29c2920de33
Jeff Palin <jeffpalin@gmail.com>
2017-09-12 15:29:14 -0700

    initial commit

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+    consumer_key: 'kDyjZoML30L8HPPIfHeD1QS8v',
+    consumer_secret: 'I6PEFnb82x6PxNAssVkH1TMoxOKpwisImSEUDI6SOXNeoDEFWz',
+    access_token_key: '907724086838575104-TZK4f8ztYW0RpIp86yebaUtDmVQVQuR',
+    access_token_secret: 'tCXxsD6cX3McOMu78IKLMBLV8x9H7TNZYJFE2fTg4lCaC',
 }
 
 module.exports = twitterKeys;
